455a032a3484da18bb6b8d38ebc2d09b5f102def
Abu Sayad Hussain <abusayadhussain@gmail.com>
2019-07-14 13:03:03 +0600

    add mongodb as a database to the project

--- app.js
+++ app.js
@@ -1,38 +1,82 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),
+    mongoose   = require("mongoose");
 
+mongoose.connect('mongodb://localhost:27017/car_parking', {useNewUrlParser: true});    
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set("view engine", "ejs");
 
-var cars = [
-    {name: "BMV", model:"M340"},
-    {name: "2020 Acura" , model: "TLX"},
-    {name: "2020 Hyundai" ,model: "Elantra"}
-];
+//Schema Setup
+var carSchema = new mongoose.Schema({
+    name: String,
+    model: String,
+    license : String
+});
+
+var Car = mongoose.model("car", carSchema);
+
+// car.create({
+//     name: "Hyundai", 
+//     model:"Venue"
+// }, function(err, car){
+//     if(err){
+//         console.log(err);
+//     } else{
+//         console.log("Newly created car");
+//         console.log(car);
+//     }
+// });
+
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/cars", function(req, res){
+    //get all the cars from the DB
+    Car.find({}, function(err, allCars){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("index", {cars:allCars});
+        }
+    });
     
-
-    res.render("cars", {cars:cars});
 });
 
 app.post("/cars", function(req,res){
     var name = req.body.name;
     var model = req.body.model;
-    var newCar = {name: name, model: model};
+    var license = req.body.license;
+    var newCar = {name: name, model: model, license: license};
 
-    cars.push(newCar);
-    res.redirect("/cars");
+    //Create a new car and send it to DB
+    Car.create(newCar, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else{
+            res.redirect("/cars");
+        }
+    });
+    
 });
 
 app.get("/cars/new", function(req,res){
     res.render("new");
+});
+
+//SHOW- show more info about the car that has been parked
+app.get("/cars/:id", function(req, res){
+    var carId = req.params.id;
+    Car.findById(carId, function(err, foundCar){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("show", {car:foundCar});
+        }
+    })
+    
 });
 
 app.listen(3000, function(){
