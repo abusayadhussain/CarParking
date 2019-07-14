6af72b47dd8110425ee738f6a29b84d8db5fd6dc
Abu Sayad Hussain <abusayadhussain@gmail.com>
2019-07-14 19:27:24 +0600

    add authentication to the project

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-var express    = require("express"),
-    app        = express(),
-    bodyParser = require("body-parser"),
-    mongoose   = require("mongoose"),
-    Car        = require("./models/car");
+var express       = require("express"),
+    app           = express(),
+    bodyParser    = require("body-parser"),
+    mongoose      = require("mongoose"),
+    Car           = require("./models/car"),
+    passport      = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User          = require("./models/user");
 
 mongoose.connect('mongodb://localhost:27017/car_parking', {useNewUrlParser: true});    
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+//Passport Configuration
+app.use(require("express-session")({
+    secret: "Code for 12 hours is tiring but pleasing",
+    resave: false,
+    saveUninitialized: false
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-// car.create({
-//     name: "Hyundai", 
-//     model:"Venue"
-// }, function(err, car){
-//     if(err){
-//         console.log(err);
-//     } else{
-//         console.log("Newly created car");
-//         console.log(car);
-//     }
-// });
-
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -73,6 +79,53 @@ app.get("/cars/:id", function(req, res){
     
 });
 
+//Auth routes
+
+//Show Register form
+app.get("/register", function(req,res){
+    res.render("register");
+});
+
+//Sign up post route
+app.post("/register", function(req,res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.redirect("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/cars");
+        });
+    });
+});
+
+//Show login form 
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+//Handling login logic
+app.post("/login", passport.authenticate("local", 
+{
+    successRedirect: "/cars",
+    failureRedirect: "/login"
+}),function(req, res) {
+
+});
+
+app.get("/logout",function(req,res){
+    req.logOut();
+    res.redirect("/cars");
+});
+
+function isLoggedIn(req,res,next){
+    if(User.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
 app.listen(3000, function(){
     console.log("The Car Parking Server has started");
-})
+});
